76dd21100cbeb511566031c6237703a17e0474a6
sudarshan <sudarshanlnx@gmail.com>
2022-04-16 08:57:28 +0530

    module-v1

---
+++ dev/terraform.tfvars
@@ -0,0 +1,6 @@
+sgname      = "dev12-sg-21"
+cidr        = ["0.0.0.0/0"]
+mytag       = "dev_env"
+amiid       = "ami-0acc9b3bb6efb1f5d"
+machinetype = "t2.micro"
+keyname     = "A_10-30_keypair"
---
+++ dev/variables.tf
@@ -0,0 +1,13 @@
+variable "sgname" {
+  sensitive = true #it will not display the name
+}
+
+variable "cidr" {}
+
+variable "mytag" {}
+
+variable "amiid" {}
+
+variable "machinetype" {}
+
+variable "keyname" {}
