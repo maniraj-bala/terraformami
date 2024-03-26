b8a28535cbe5f77e16d54e7c2bf1185685f6cbd9
maniraj-bala <119467326+maniraj-bala@users.noreply.github.com>
2024-03-26 18:59:54 +0530

    securitygroupchange

--- terraform/ec2basic.tf
+++ terraform/ec2basic.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami             = "ami-0a70b9d193ae8a799 "
   instance_type   = "t3.micro"
   key_name        = "devopsresearch&learn"
-  security_groups = ["default"]
+  security_groups_id = ["sg-0fccdb30deb1f4d14"]
 
   tags = {
     Name = "webserver"
