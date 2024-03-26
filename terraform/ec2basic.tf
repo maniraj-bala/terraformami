9c22777c308240b2511b2b57390ef23574145533
maniraj-bala <119467326+maniraj-bala@users.noreply.github.com>
2024-03-26 19:02:49 +0530

    cpuchange

--- terraform/ec2basic.tf
+++ terraform/ec2basic.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
   ami             = "ami-0a70b9d193ae8a799 "
-  instance_type   = "t3.micro"
+  instance_type   = "t2.micro"
   key_name        = "devopsresearch&learn"
-  security_groups_id = ["sg-0fccdb30deb1f4d14"]
+  security_groups_ids = ["sg-0fccdb30deb1f4d14"]
 
   tags = {
     Name = "webserver"
