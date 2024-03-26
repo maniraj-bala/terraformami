b947c80684e0c2d3ce9ccf5283f97dde7748403a
maniraj-bala <119467326+maniraj-bala@users.noreply.github.com>
2024-03-26 19:10:35 +0530

    sgchage

--- terraform/ec2basic.tf
+++ terraform/ec2basic.tf
@@ -2,8 +2,7 @@ resource "aws_instance" "web" {
   ami             = "ami-0a70b9d193ae8a799 "
   instance_type   = "t2.micro"
   key_name        = "devopsresearch&learn"
-  security_groups_ids = ["sg-0fccdb30deb1f4d14"]
-
+  security_groups = ["terraform"]
   tags = {
     Name = "webserver"
     Env  = "Production"
