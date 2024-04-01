8d7cf01c17a1fa5faf0543e4ec638ba9e28e799f
maniraj-bala <119467326+maniraj-bala@users.noreply.github.com>
2024-04-01 12:05:03 +0530

    ebsvolumeattch

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,21 +1,21 @@
-data "aws_ami" "ami-test1" {
-  most_recent = true
-  owners      = ["self"]
-  filter {
-    name   = "name"
-    values = var.value
-  }
-}
-
-resource "aws_instance" "hemant" {
-  ami           = data.aws_ami.ami-test1.id
-  instance_type = var.machinetype
-  key_name      = "devjenkins"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
+#data "aws_ami" "ami-test1" {
+#  most_recent = true
+#  owners      = ["self"]
+#  filter {
+#    name   = "name"
+#    values = var.value
+#  }
+#}
+#
+#resource "aws_instance" "hemant" {
+#  ami           = data.aws_ami.ami-test1.id
+#  instance_type = var.machinetype
+#  key_name      = "devjenkins"
+#
+#  tags = {
+#    Name = "HelloWorld"
+#  }
+#}
 
 
 
--- terraform/ec2basic.tf
+++ terraform/ec2basic.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami             = "ami-0a70b9d193ae8a799 "
+  ami             = "ami-0a70b9d193ae8a799"
   instance_type   = "t2.micro"
   key_name        = "devopsresearch&learn"
   security_groups = ["terraform"]
