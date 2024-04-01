c2987b55dcf0b9c87ef5c7c850fbe5259b06aa7d
maniraj-bala <119467326+maniraj-bala@users.noreply.github.com>
2024-04-01 12:34:33 +0530

    ebsworkingcode

---
+++ terraform/ebs.tf
@@ -0,0 +1,22 @@
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.web.id
+}
+
+resource "aws_instance" "web" {
+  ami             = "ami-0a70b9d193ae8a799"
+  instance_type   = "t2.micro"
+  key_name        = "devopsresearch&learn"
+  security_groups = ["terraform"]
+  tags = {
+    Name = "webserver"
+    Env  = "Production"
+  }
+}
+
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2a"
+  size              = 1
+}
