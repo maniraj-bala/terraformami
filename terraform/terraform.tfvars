06ca1612fa208a43f91d7fdca55f2d208e75f590
maniraj-bala <119467326+maniraj-bala@users.noreply.github.com>
2024-04-01 18:37:22 +0530

    variable

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,1 +1,2 @@
-machinetype = "t2.micro"
+ami = "ami-0a70b9d193ae8a799"
+instance_type = "t2.micro"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,8 @@
 
 variable "ami" {
-  default = "ami-0a70b9d193ae8a799"
+
 }
 variable "instance_type" {
-  default = "t2.micro"
+
 }
 
